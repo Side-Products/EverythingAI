84b5426c12156727b22cfd87a63ff154c55c69e8
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2023-06-21 22:50:42 +0530

    interim tool card and tools carousel component

--- src/components/Tools/ToolCard.js
+++ src/components/Tools/ToolCard.js
@@ -3,18 +3,17 @@ import MarqueeText from "../ui/MarqueeText";
 import styles from "@/styles/ToolCard/ToolCard.module.css"
 
 export default function ToolCard({ tool, onClick }) {
-	console.log("tool::", tool);
 	/**
 	 * @returns background and text color tw-classes w.r.t pricing name
 	 */
 	const getPricingChipClass = () => {
-		switch (tool.pricing.name){
+		switch (tool.pricing.name.split(" ")[0]){
 			case 'Free':
 				return 'text-green-800 bg-green-300';
-			case 'Freemium':
-				return 'text-purple-800 bg-purple-300';
 			case 'Premium':
 				return 'text-yellow-800 bg-yellow-300';
+			default:
+				return 'text-purple-800 bg-purple-300';
 		}
 	}
 
@@ -23,7 +22,7 @@ export default function ToolCard({ tool, onClick }) {
 			<Image src={tool.image} width={533} height={300} alt="tool image" className="rounded-tl-lg rounded-tr-lg" />
 
 			<div className="p-5">
-				{tool.name.length > 10? 
+				{tool.name.length < 10? 
 					<p className="text-lg font-semibold">{tool.name}</p>
 					:
 					<MarqueeText text={tool.name} classes={'text-lg font-semibold'} marqueeWidth={'w-[125px]'}/>
