6c608c0ffbee85dc1b3c5cff04442ba15fcc41ae
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2023-06-23 00:16:49 +0530

    ui touch ups

--- src/components/Tools/ToolUtils/ExploreTools.js
+++ src/components/Tools/ToolUtils/ExploreTools.js
@@ -2,8 +2,8 @@ import ToolsCarousel from "../ToolsCarousel";
 
 export default function ExploreTools({tools}){
     return (
-        <div className="mb-8">
-            <h3 className="mt-32 text-3xl font-medium font-secondary text-primary-500">Explore Similar Tools 🛠️</h3>
+        <div className="mt-36">
+            <h3 className="text-3xl font-medium font-secondary text-primary-500">Explore Similar Tools 🛠️</h3>
             <ToolsCarousel tools={tools}/>
         </div>
     );
