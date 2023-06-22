baba67d043b1bf191d553edbc07a1be71b7d8377
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2023-06-23 00:15:28 +0530

    ui touch ups

--- src/components/Tools/ToolUtils/ExploreTools.js
+++ src/components/Tools/ToolUtils/ExploreTools.js
@@ -3,7 +3,7 @@ import ToolsCarousel from "../ToolsCarousel";
 export default function ExploreTools({tools}){
     return (
         <div className="mb-8">
-            <h3 className="mt-32 mb-5 text-3xl font-medium font-secondary text-primary-500">Explore Similar Tools 🛠️</h3>
+            <h3 className="mt-32 text-3xl font-medium font-secondary text-primary-500">Explore Similar Tools 🛠️</h3>
             <ToolsCarousel tools={tools}/>
         </div>
     );
--- src/components/Tools/ToolUtils/ToolIntro.js
+++ src/components/Tools/ToolUtils/ToolIntro.js
@@ -2,21 +2,31 @@ import Image from "next/image";
 import ToolSocials from "./ToolIntroUtils/ToolSocials";
 import ToolPill from "./ToolIntroUtils/ToolPill";
 import { getPricingChipClass } from "@/utils/Helpers";
-//Is one social media url compulsory?
-//also about the loading context state to automaticaly set to true when fetching in reduce
+import Link from "next/link";
+import Button from "@/components/ui/Button";
+
 export default function ToolIntro({tool}){
     const createdDate = new Date(tool.createdAt);
+    const hasSocials = () => {
+        return ((tool.instagram || tool.twitter || tool.linkedin || tool.youtube).length > 0);
+    }
+
     return (
         <>
             <div className="flex mt-8">
                 <Image className="rounded-lg" width={640} height={360} src={tool.image} priority alt="tool image"/>
                 <div className="flex flex-col flex-1 py-5 ml-12">
-                    <div className="flex items-center mb-5 space-x-2">
-                        <a href={tool.url} target="_blank" rel="noreferrer" className="text-3xl font-semibold cursor-pointer font-secondary">{tool.name}</a>
-                        <Image src={'/verified_tick.svg'} width={22} height={22} alt="verified tick"/>
-                        {/* <a className="ml-auto" href={tool.url} target="_blank" rel="noreferrer">
-                            <span className="cursor-pointer material-symbols-outlined hover:text-primary-600 text-primary-400">travel_explore</span>
-                        </a> */}
+                    <div className="flex items-center justify-between mb-5">
+                        <div className="flex items-center space-x-2">
+                            <a href={tool.url} target="_blank" rel="noreferrer" className="text-3xl font-semibold cursor-pointer font-secondary hover:text-primary-600">{tool.name}</a>
+                            <Image src={'/verified_tick.svg'} width={22} height={22} alt="verified tick"/>
+                        </div>
+                        <Link href={tool?.url} target="_blank" rel="noopener noreferrer">
+                            <Button type="button">
+                                <i className="mr-2 text-base fa-solid fa-arrow-up-right-from-square text-light-100"></i>
+                                Visit Site
+                            </Button>
+                        </Link>
                     </div>
                     <p className="mb-5 font-medium">{tool.oneLiner}</p>
                     <div className="flex mb-3 space-x-3">
@@ -29,10 +39,11 @@ export default function ToolIntro({tool}){
                         chipStyle={getPricingChipClass(tool.pricing.name)}
                         tooltip={tool.pricing.meta}
                     />
-
-                    <div className="mt-auto">
-                        <ToolSocials tool={tool}/>
-                    </div>
+                    {hasSocials() && 
+                        <div className="mt-auto">
+                            <ToolSocials tool={tool}/>
+                        </div>
+                    }
                 </div>
             </div>
         </>
