b463d5928c485b74fa12e53915caea237dd55fff
pushpit07 <pushpit07@gmail.com>
2023-06-26 17:25:35 +0530

    Added search
    Signed-off-by: pushpit07 <pushpit07@gmail.com>

--- src/components/Cards/ToolCard.js
+++ src/components/Cards/ToolCard.js
@@ -74,7 +74,7 @@ export default function ToolCard({ tool }) {
 										<span className="mt-[3px]">
 											<Tooltip
 												labelText={
-													<span className={"ml-1 text-[14px] material-symbols-outlined " + getPricingChipClass(tool?.pricing?.name)}>
+													<span className={"ml-1 text-[10px] material-symbols-outlined " + getPricingChipClass(tool?.pricing?.name)}>
 														info
 													</span>
 												}
