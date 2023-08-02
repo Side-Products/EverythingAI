c9224efc53374e7ae03a71374ca557906c200776
pushpit07 <pushpit07@gmail.com>
2023-08-02 18:03:02 +0530

    Added google tag manager
    Signed-off-by: pushpit07 <pushpit07@gmail.com>

--- src/lib/google-analytics/index.js
+++ src/lib/google-analytics/index.js
@@ -1,3 +1,10 @@
+// export const pageview = (url) => {
+// 	window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID, { path_url: url });
+// };
+
 export const pageview = (url) => {
-	window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID, { path_url: url });
+	window.dataLayer.push({
+		event: "pageview",
+		page: url,
+	});
 };
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -27,8 +27,22 @@ function App({ Component, pageProps, session, router }) {
 
 	return (
 		<>
-			{/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+			{/* <!-- Google Tag Manager --> */}
 			<Script
+				id="gtag-base"
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `
+						(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+						})(window,document,'script','dataLayer','GTM-PT8VNFD');
+					`,
+				}}
+			/>
+			{/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+			{/* <Script
 				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
 				strategy="afterInteractive"
 				onError={(err) => {}}
@@ -42,7 +56,7 @@ function App({ Component, pageProps, session, router }) {
                     page_path: window.location.pathname,
                     });
                 `}
-			</Script>
+			</Script> */}
 
 			<Script src="https://kit.fontawesome.com/8f4546bba1.js" crossOrigin="anonymous"></Script>
 			<Script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/tw-elements.umd.min.js"></Script>
