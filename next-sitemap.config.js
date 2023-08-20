523de812e45a8b3dd3f0891c208d4d7ab3d92428
pushpit07 <pushpit07@gmail.com>
2023-08-20 17:16:55 +0530

    Added Sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,13 +1,15 @@
 const product_url = "https://everythingai.club";
+// const product_url = "http://localhost:3000";
 
 module.exports = {
 	siteUrl: product_url,
 	generateRobotsTxt: true,
-	exclude: ["/admin/*"],
+	exclude: ["/admin/*", "/404"],
 	robotsTxtOptions: {
 		policies: [
 			{ userAgent: "*", disallow: "/admin/*" },
 			{ userAgent: "*", allow: "/" },
 		],
+		additionalSitemaps: [`${product_url}/sitemap.xml`, `${product_url}/sitemap-0.xml`, `${product_url}/server-sitemap.xml`],
 	},
 };
