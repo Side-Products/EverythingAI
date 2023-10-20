e86ae534dd1ccb5bd94fd464533a4dc00b82a161
Deepak <deepakkomma@gmail.com>
2023-10-20 19:13:44 +0530

    removed caching in apollo client

--- src/lib/apollo.js
+++ src/lib/apollo.js
@@ -3,4 +3,14 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 export const client = new ApolloClient({
   uri: `${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}`,
   cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "ignore",
+    },
+    query: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "all",
+    },
+  },
 });
--- src/pages/blogs/index.jsx
+++ src/pages/blogs/index.jsx
@@ -37,7 +37,6 @@ export async function getStaticProps() {
     props: {
       posts,
     },
-    revalidate: 60,
   };
 }
 
