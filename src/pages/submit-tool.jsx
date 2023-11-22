e49ea1a4f9bd47fd6fa3a5106fb3dc766e0cfd7e
spantheslayer <supriyosarkar282@gmail.com>
2023-11-22 17:00:10 +0530

    test

--- src/pages/submit-tool.jsx
+++ src/pages/submit-tool.jsx
@@ -14,7 +14,7 @@ export default function Submit() {
       <h1 className="text-[40px] sm:text-[60px] font-bold text-center tracking-[-1px] text-gradient-primary-tr leading-[1.2em]">
         Submit Tool
       </h1>
-      <SubmitTool />
+      {/* <SubmitTool /> */}
     </PageWrapper>
   );
 }
